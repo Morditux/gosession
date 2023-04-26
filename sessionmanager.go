64ed9c35ffbce3252994d02edc5e802103353cc2
Kavoos Bojnourdi <kb@triscale-innov.com>
2023-04-26 11:19:57 +0200

    Fixe interface

--- sessionmanager.go
+++ sessionmanager.go
@@ -1,13 +1,13 @@
 package sessions
 
 type SessionManager interface {
-	CreateSession(userName string, isAdmin bool) (Session, bool)
-	Get(key string) (Session, error)
+	CreateSession(userName string, isAdmin bool) (*Session, bool)
+	Get(key string) (*Session, error)
 	GetSessionCount() int
 	Clean()
-	Add(session Session)
+	Add(session *Session)
 	Remove(key string)
 	Exists(key string) bool
-	FromBinary(data []byte) Session
-	ToBinary(session Session) []byte
+	FromBinary(data []byte) *Session
+	ToBinary(session *Session) []byte
 }
